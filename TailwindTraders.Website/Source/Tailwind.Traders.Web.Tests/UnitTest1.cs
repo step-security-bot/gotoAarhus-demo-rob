d56ba53b211ad2a65c387fd6cd258c53c710f0f9
Marcel de Vries <vriesmarcel@hotmail.com>
2022-11-11 19:50:18 +0100

    Add unit test project with passing tests

---
+++ TailwindTraders.Website/Source/Tailwind.Traders.Web.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+namespace Tailwind.Traders.Web.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_website_shows_Discounts()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void Test_website_has_highlighted_items()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void Test_website_has_highlight_bar_data()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void Test_website_has_shoppingcart()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
